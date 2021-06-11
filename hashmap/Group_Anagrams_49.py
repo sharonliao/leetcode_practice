d5f4e28f5d5c31c148bd86373c6ca3960aabeb75
Xiaoyun Liao <sharon.liaoxy@gmail.com>
2021-06-11 15:05:15 -0400

    update the soution of 49

--- hashmap/Group_Anagrams_49.py
+++ hashmap/Group_Anagrams_49.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class Solution(object):
     def groupAnagrams(self, strs):
         """
@@ -5,25 +8,17 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         # 1. get the code for each str
-        # 2. put (code, index of result) in dict
-        # 3. the strs have same code put in same place of result list.
-        result_list = []
+        # 2. put (code, group of strs) in dict
+        # 4. time complexity O(NK), space(NK)
         code_dict = {}
         for str in strs:
             code = self.getCode(str);
-            if code_dict.get(code) is not None :
-                index = code_dict.get(code)
-                print(f"index: {index}")
-                result_list[index].append(str)
+            if code_dict.get(code) is not None:
+                code_dict.get(code).append(str)
             else:
-                print(f"dict: {code_dict}")
-                index = len(code_dict)
-                code_dict[code]=index;
-                result_list.append([str])
-                print(f"new index: {index}")
-            print(result_list)
-
-        return result_list
+                code_dict[code] = [str]
+        print(f"result:{code_dict.values()}")
+        return code_dict.values()
 
     def  getCode(self, s):
         """
@@ -42,6 +37,35 @@ class Solution(object):
         str_of_ints = ".".join(string_ints)
         print(f"str :{s},code: {str_of_ints}")
         return str_of_ints
+
+
+    def  getCode_tuple(self, s):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        # 1.using a list which size is 26 to store the number of each lowercase English letters
+        # 2.using ascii-97 as index of list
+        count_list = [0]*26
+        for c in s :
+            index = ord(c)-97
+            count_list[index] += 1;
+        return tuple(count_list)
+
+
+
+    def groupAnagrams_advance(strs):
+        ans = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            # tuple(count) is hashable
+            # directly store group result in the valus
+            ans[tuple(count)].append(s)
+        return ans.values()
+
 
 
 
