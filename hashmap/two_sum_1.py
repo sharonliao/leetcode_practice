661207f35a6a1309d01df988000607399250486c
Xiaoyun Liao <sharon.liaoxy@gmail.com>
2021-06-12 15:12:24 -0400

    add solution of 1

---
+++ hashmap/two_sum_1.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    # Input: nums = [2,7,11,15], target = 9
+    # Output: [0,1]
+    # Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+    # time complexity O(N)
+    # space complexity O(N)
+
+
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        remain_dict = {}
+        for index,value in enumerate(nums):
+            if remain_dict.get(value) is not None:
+                return [index,remain_dict.get(value)]
+            else:
+                remain_dict[target-value] = index;
